39f951e8efe51fccfbd7e56a7cd861667ed1be0c
Jōsh Mysoré <joshmysore@Joshs-MacBook-Pro-10.local>
2025-11-01 16:27:51 -0400

    Update CNAME to correct domain joshmysore.me

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/personal-website' : '',
   images: {
     unoptimized: true,
   },
